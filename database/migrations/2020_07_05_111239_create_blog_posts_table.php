81e4a0294d38c1ba3d9ee46be61ab4ced28a4b7a
unknown <kimiriecs@gmail.com>
2020-07-05 16:33:05 +0300

    blog_categories_table & BlogCategory_model, blog_posts_table & BlogPost_model

---
+++ database/migrations/2020_07_05_111239_create_blog_posts_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('blog_category_id');
+
+            $table->string('slug')->unique();
+            $table->string('title'); 
+
+            $table->text('excerpt')->nullable();
+
+            $table->text('content_raw')->nullable();
+            $table->text('content_html')->nullable();
+
+            $table->boolean('is_published')->nullable()->default(false);
+            $table->timestamp('published_at')->nullable();
+            
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('is_published');
+        });
+        Schema::table('blog_posts', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('blog_category_id')->references('id')->on('blog_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+}
