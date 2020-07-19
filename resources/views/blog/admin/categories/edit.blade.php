0fc88271209578ffa39539b839149659c07cb320
unknown <kimiriecs@gmail.com>
2020-07-19 08:33:23 +0300

    BlogCategoryCreateRequest

--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -4,6 +4,12 @@
 
 {{--  @php /**@var \App\Models\BlogCategory $item*/ @endphp  --}}
 
+{{--  @if ($item->exists)
+<form action="{{ route('blog.admin.categories.update', $item->id) }}" method="POST">
+    @method('PATCH')
+@else
+<form action="{{ route('blog.admin.categories.store', $item->id) }}" method="POST">  
+@endif  --}}
 <form action="{{ route('blog.admin.categories.update', $item->id) }}" method="POST">
     @method('PATCH')
     @csrf
--- resources/views/blog/admin/categories/index.blade.php
+++ resources/views/blog/admin/categories/index.blade.php
@@ -5,9 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <button type="button" class="btn btn-primary" href="{{ route('blog.admin.categories.create') }}">
+                <a type="button" class="btn btn-primary" href="{{ route('blog.admin.categories.create') }}">
                     <span class="">Добавть категорию</span>
-                </button>
+                </a>
             </nav>
             <div class="card">
                 <div class="card-body">
