04e4a42712de417c2845fa466c51c44ecc14f4f4
Your Name <you@example.com>
2020-08-06 00:16:48 +0300

    BlogPost update

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -7,9 +7,31 @@ use App\Models\BlogCategory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+
+/**
+ * Class BlogPost
+ * 
+ * @package App\Models
+ * @property App\Models\BlogCategory    $blogCategory
+ * @property App\Models\User            $user
+ * @property string                     $title
+ * @property string                     $slug
+ * @property string                     $blog_category_id
+ * @property string                     $excerpt
+ * @property boolean                     $is_published
+ */
 class BlogPost extends Model
 {
     use SoftDeletes;
+
+    protected $fillable = 
+    [
+        'title',
+        'slug',
+        'blog_category_id',
+        'excerpt',
+        'is_published',
+    ];
 
     /**
      * Категория статьи
