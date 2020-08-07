241753c947bb1662872875da4d5e82378f3f9dc4
Your Name <you@example.com>
2020-08-07 16:53:04 +0300

    welcome.blade title fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,7 +91,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ config('app.name', 'Laravel') }}
                 </div>
 
                 <div class="links">
