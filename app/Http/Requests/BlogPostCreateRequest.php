aebdfc9ae81be304b0588f002480bfcff364ca94
Your Name <you@example.com>
2020-08-08 20:30:12 +0300

    BlogPostCreateRequest fix

--- app/Http/Requests/BlogPostCreateRequest.php
+++ app/Http/Requests/BlogPostCreateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class BlogPostUpdateRequest extends FormRequest
+class BlogPostCreateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
